23884e79e4fd0bfd09e3ffe5d318f2a751988b19
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2026-06-10 17:48:50 +0200

    simplify uname in single header

--- includes/uname.h
+++ includes/uname.h
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 17:33:31 by mgama             #+#    #+#             */
-/*   Updated: 2026/06/10 17:42:30 by mgama            ###   ########.fr       */
+/*   Updated: 2026/06/10 17:48:23 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,23 @@ struct utsname_s {
 	char date[32];
 };
 
-extern const struct utsname_s kuname;
+#define KERNEL_NAME     "Galileo"
+#define KERNEL_VERSION  "0.1.0-alpha"
+#define KERNEL_DATE     __DATE__ " " __TIME__
+
+#if defined(__x86_64__)
+	#define KERNEL_ARCH "x86_64"
+#elif defined(__i386__)
+	#define KERNEL_ARCH "i386"
+#else
+	#define KERNEL_ARCH "unknown"
+#endif
+
+const struct utsname_s kuname = {
+	.sysname = KERNEL_NAME,
+	.release = KERNEL_VERSION,
+	.machine = KERNEL_ARCH,
+	.date    = KERNEL_DATE
+};
 
 #endif /* UNAME_H */
